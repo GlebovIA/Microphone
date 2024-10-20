2f7ea121ac4eb48294164f54573b9b005de6dc51
Kolovratik <Kolovratik@HOME-PC>
2024-10-20 17:04:59 +0300

    Объявление переменных

--- app/src/main/java/com/example/microphone/MainActivity.java
+++ app/src/main/java/com/example/microphone/MainActivity.java
@@ -2,9 +2,14 @@ package com.example.microphone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MediaRecorder mediaRecorder;
+    private MediaPlayer mediaPlayer;
+    private String fileName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
